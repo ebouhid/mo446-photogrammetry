d981d1457e4fadd0a61d46a023c83370165d6140
Eduardo Bouhid <eduardobouhid.neto@gmail.com>
2025-09-17 09:50:10 -0300

    fix: save database to correct work dir

--- run.sh
+++ run.sh
@@ -6,16 +6,16 @@
 # to be processed.
 
 colmap feature_extractor \
-   --database_path database.db \
+   --database_path "$1/database.db" \
    --image_path "$1/images"
 
 colmap exhaustive_matcher \
-   --database_path database.db
+   --database_path "$1/database.db"
 
 mkdir "$1/sparse"
 
 colmap mapper \
-    --database_path database.db \
+    --database_path "$1/database.db" \
     --image_path "$1/images" \
     --output_path "$1/sparse"
 
