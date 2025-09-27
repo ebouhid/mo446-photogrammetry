88660a702123eacae61687b0d84607ad61e0e439
lucasrdrgs <lucasrodriguespimentel@gmail.com>
2025-09-27 16:33:54 -0300

    Dense cloud

--- gen-ply-seq.sh
+++ gen-ply-seq.sh
@@ -7,10 +7,14 @@
 
 colmap feature_extractor \
    --database_path "$1/database.db" \
-   --image_path "$1/images"
+   --image_path "$1/images" \
+   --SiftExtraction.estimate_affine_shape=true \
+   --SiftExtraction.domain_size_pooling=true \
 
 colmap sequential_matcher \
-   --database_path "$1/database.db"
+   --database_path "$1/database.db" \
+   --SiftMatching.max_ratio=0.7 \
+   --SiftMatching.max_distance=0.7 \
 
 mkdir "$1/sparse"
 
@@ -23,3 +27,23 @@ colmap model_converter \
     --input_path "$1/sparse/0" \
     --output_path "$1/sparse/0/points3D.ply" \
     --output_type PLY
+
+mkdir -p "$1/dense"
+
+colmap image_undistorter \
+    --image_path "$1/images" \
+    --input_path "$1/sparse/0" \
+    --output_path "$1/dense" \
+    --output_type COLMAP
+
+colmap patch_match_stereo \
+    --workspace_path "$1/dense" \
+    --workspace_format COLMAP \
+    --PatchMatchStereo.geom_consistency=true \
+    --PatchMatchStereo.gpu_index=0 \
+	--PatchMatchStereo.max_image_size 1000 \
+	--PatchMatchStereo.cache_size 6
+
+colmap stereo_fusion \
+    --workspace_path "$1/dense" \
+    --output_path "$1/dense/fused.ply"
